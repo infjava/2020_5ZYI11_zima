142412c8526051ac32d70068c7fde9de6e364248
Jan Janech <jan.janech@kst.uniza.sk>
2020-10-22 08:04:37 +0200

    04a - vysledok cvicenia, rozrobene hodiny so segmentovym displejom

---
+++ digitalneHodiny/CiselnyDisplej.java
@@ -0,0 +1,68 @@
+/**
+ * Trieda reprezentujuca jeden ciselny displej v digitalnych hodinach.
+ * Stara sa o zmenu hodnoty v zadanych hraniciach a o formatovanie
+ * cisla do tvaru dvojcifernej hodnoty.
+ */
+public class CiselnyDisplej {
+    private int hornaHranica;
+    private int dolnaHranica;
+    private int hodnota;
+    
+    /**
+     * Inicializuje ciselny displej na hodnotu 0. Horna hranica sa pouzije
+     * ta, co zada pouzivatel v parametri.
+     * @param pthis.hornaHranica Predstavuje cislo, ktore hodnota ciselneho
+     * displeja nemoze dosiahnut.
+     */
+    public CiselnyDisplej(int dolnaHranica, int hornaHranica) {
+        this.hornaHranica = hornaHranica;
+        this.dolnaHranica = dolnaHranica;
+        this.hodnota = dolnaHranica;
+    }
+    
+    /**
+     * Vrati aktualnu hodnotu ciselneho displeja vo forme celeho cisla typu
+     * int.
+     */
+    public int getHodnota() {
+        return this.hodnota;
+    }
+    
+    /**
+     * Nastavi novu hodnotu ciselneho displeja vo forme celeho cisla typu int.
+     * Kontroluje platnost zadavaneho cisla. V pripade, ze je mimo rozsah,
+     * nechava povodnu hodnotu.
+     * 
+     * @param hodnota hodnota, ktora sa ma nastavit.
+     */
+    public void setHodnota(int hodnota) {
+        if (hodnota >= this.dolnaHranica) {
+            if (hodnota < this.hornaHranica) {
+                this.hodnota = hodnota;
+            }
+        }
+    }
+    
+    /**
+     * Vrati hodnotu ciselneho displeja vo forme retazca, pricom hodnota je
+     * vzdy vo forme dvojciferneho cisla s pripadnou uvodnou nulou.
+     */
+    public String getHodnotaAkoRetazec() {
+        if (this.hodnota < 10) {
+            return "0" + this.hodnota;
+        } else {
+            return "" + this.hodnota;
+        }
+    }
+    
+    /**
+     * Zvacsi hodnotu na ciselnom displeji o hodnotu jedna. Ak dosiahne hornu
+     * hranicu, pokracuje znovu od nuly.
+     */
+    public void krok() {
+        this.hodnota = this.hodnota + 1;
+        if (this.hodnota == this.hornaHranica) {
+            this.hodnota = this.dolnaHranica;
+        }
+    }
+}
---
+++ digitalneHodiny/DigitalneHodiny.java
@@ -0,0 +1,55 @@
+/**
+ * Jednoduche digitalne hodiny pracujuce s virtualnym casom.
+ * Pamataju si hodiny a minuty.
+ */
+public class DigitalneHodiny {
+    private CiselnyDisplej hodiny;
+    private CiselnyDisplej minuty;
+    private CiselnyDisplej sekundy;
+    
+    /**
+     * Inicializuje digitalne hodiny. Cas zacina plynut od
+     * polnoci (00:00).
+     */
+    public DigitalneHodiny() {
+        this.hodiny = new CiselnyDisplej(1, 13);
+        this.minuty = new CiselnyDisplej(0, 60);
+        this.sekundy = new CiselnyDisplej(0, 60);
+    }
+    
+    /**
+     * Vykonava posunutie virtualneho casu o jednu minutu.
+     */
+    public void tik() {
+        this.sekundy.krok();
+        
+        if (this.sekundy.getHodnota() == 0) {
+            this.minuty.krok();
+            
+            if (this.minuty.getHodnota() == 0) {
+                this.hodiny.krok();
+            }
+        }
+    }
+    
+    /**
+     * Nastavi virtualny cas na zadanu hodnotu.
+     * 
+     * @param hodina Hodina na ktoru sa ma cas nastavit.
+     * @param minuta Minuta na ktoru sa ma cas nastavit.
+     */
+    public void setCas(int hodina, int minuta, int sekunda) {
+        this.hodiny.setHodnota(hodina);
+        this.minuty.setHodnota(minuta);
+        this.sekundy.setHodnota(sekunda);
+    }
+    
+    /**
+     * Vrati cas vo forme retazca.
+     */
+    public String getCas() {
+        return this.hodiny.getHodnotaAkoRetazec() + ":"
+            + this.minuty.getHodnotaAkoRetazec() + ":"
+            + this.sekundy.getHodnotaAkoRetazec();
+    }
+}
