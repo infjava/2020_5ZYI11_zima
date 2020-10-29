617ecb5c363718a08200e83b6e96db690dbc478c
Jan Janech <jan.janech@kst.uniza.sk>
2020-10-29 08:58:32 +0100

    05b - trieda pre loptu

---
+++ pong/Lopta.java
@@ -0,0 +1,14 @@
+public class Lopta {
+    private Kruh zobrazenie;
+    
+    public Lopta() {
+        this.zobrazenie = new Kruh();
+        
+        this.zobrazenie.zmenPriemer(10);
+        
+        this.zobrazenie.posunVodorovne(125);
+        this.zobrazenie.posunZvisle(85);
+        
+        this.zobrazenie.zobraz();
+    }
+}
