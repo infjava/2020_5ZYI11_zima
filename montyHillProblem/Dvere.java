2af47617a24ebf330df0a7f541d1d5c3de910187
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-19 08:31:37 +0100

    08a - trieda Dvere podla navrhu

---
+++ montyHillProblem/Dvere.java
@@ -0,0 +1,11 @@
+public class Dvere {
+    private boolean obsahujuVyhru;
+    private boolean otvorene;
+    private boolean vybrane;
+    
+    public Dvere(boolean obsahujuVyhru) {
+        this.obsahujuVyhru = obsahujuVyhru;
+        this.otvorene = false;
+        this.vybrane = false;
+    }
+}
