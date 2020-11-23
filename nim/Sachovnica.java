b52dfd55bf8d5f479a25784bbf3567441533796c
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-23 15:49:14 +0100

    09a - riesenie ciernosti policka A1 - este jednoduchsia podmienka

--- nim/Sachovnica.java
+++ nim/Sachovnica.java
@@ -6,7 +6,7 @@ public class Sachovnica {
                 policko.zmenStranu(15);
                 policko.posunVodorovne(stlpec * 15 - 60);
                 policko.posunZvisle(riadok * 15 - 50);
-                if ((vyska % 2 == 0 && sirka % 2 == 0) || (vyska % 2 == 0 && sirka % 2 != 0)) {
+                if (vyska % 2 == 0) {
                     if ((riadok + stlpec) % 2 != 0) {
                         policko.zmenFarbu("black");
                     } else {
