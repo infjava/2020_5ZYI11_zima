3635c52d4f9e995b18fd68ed8647433c873cf36a
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-23 15:24:21 +0100

    09a - zobrazenie sachovnice

---
+++ nim/Sachovnica.java
@@ -0,0 +1,18 @@
+public class Sachovnica {
+    public Sachovnica(int sirka, int vyska) {
+        for (int riadok = 0; riadok < vyska; riadok++) {
+            for (int stlpec = 0; stlpec < sirka; stlpec++) {
+                Stvorec policko = new Stvorec();
+                policko.zmenStranu(15);
+                policko.posunVodorovne(stlpec * 15 - 60);
+                policko.posunZvisle(riadok * 15 - 50);
+                if ((riadok + stlpec) % 2 == 0) {
+                    policko.zmenFarbu("black");
+                } else {
+                    policko.zmenFarbu("white");
+                }
+                policko.zobraz();
+            }
+        }
+    }
+}
