1adb163653603b32cde9b8fbae9d20d16b33aff1
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-30 15:02:17 +0100

    10a - kamen sa pohybuje na lubovolnu poziciu

--- nim/Kamen.java
+++ nim/Kamen.java
@@ -1,7 +1,13 @@
 public class Kamen {
     private Kruh kamen;
     
+    private int poziciaRiadok;
+    private int poziciaStlpec;
+    
     public Kamen(Sachovnica sachovnica) {
+        this.poziciaRiadok = sachovnica.getVyska();
+        this.poziciaStlpec = sachovnica.getSirka();
+        
         this.kamen = new Kruh();
         this.kamen.zmenPriemer(13);
         this.kamen.posunVodorovne(-19 + (sachovnica.getSirka() - 1) * 15);
@@ -14,5 +20,18 @@ public class Kamen {
     
     public void skry() {
         this.kamen.skry();
+    }
+    
+    public void posun(int novyRiadok, int novyStlpec) {
+        int posunStlpce = novyStlpec - this.poziciaStlpec;
+        int posunRiadky = novyRiadok - this.poziciaRiadok;
+        
+        this.kamen.skry();
+        this.kamen.posunVodorovne(posunStlpce * 15);
+        this.kamen.posunZvisle(-posunRiadky * 15);
+        this.kamen.zobraz();
+        
+        this.poziciaRiadok = novyRiadok;
+        this.poziciaStlpec = novyStlpec;
     }
 }
