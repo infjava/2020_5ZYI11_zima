2813b17cf4f5b1198aaf585c7e897f5d9175a55d
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-03 08:29:29 +0100

    10b - hrac na tahu - prva moznost riesenia

--- nim/Nim.java
+++ nim/Nim.java
@@ -4,14 +4,26 @@ public class Nim {
     
     private int kamenRiadok;
     private int kamenStlpec;
+    
+    private String hracNaTahu;
+    private String prvyHrac;
+    private String druhyHrac;
 
-    public Nim(int sirkaSachovnice, int vyskaSachovnice) {
+    public Nim(int sirkaSachovnice, int vyskaSachovnice, String prvyHrac, String druhyHrac) {
         this.sachovnica = new Sachovnica(sirkaSachovnice, vyskaSachovnice);
         this.kamen = this.sachovnica.pridajKamen();
         this.sachovnica.zobraz();
         
         this.kamenRiadok = vyskaSachovnice;
         this.kamenStlpec = sirkaSachovnice;
+        
+        this.hracNaTahu = prvyHrac;
+        this.prvyHrac = prvyHrac;
+        this.druhyHrac = druhyHrac;
+    }
+    
+    public String getHracNaTahu() {
+        return this.hracNaTahu;
     }
     
     public void posunDole(int pocetPolicok) {
@@ -28,6 +40,12 @@ public class Nim {
         this.kamen.posun(novyRiadok, this.kamenStlpec);
         
         this.kamenRiadok = novyRiadok;
+        
+        if (this.hracNaTahu.equals(this.prvyHrac)) {
+            this.hracNaTahu = this.druhyHrac;
+        } else {
+            this.hracNaTahu = this.prvyHrac;
+        }
     }
     
     public void posunVlavo(int pocetPolicok) {
@@ -44,5 +62,11 @@ public class Nim {
         this.kamen.posun(this.kamenRiadok, novyStlpec);
         
         this.kamenStlpec = novyStlpec;
+        
+        if (this.hracNaTahu.equals(this.prvyHrac)) {
+            this.hracNaTahu = this.druhyHrac;
+        } else {
+            this.hracNaTahu = this.prvyHrac;
+        }
     }
 }
